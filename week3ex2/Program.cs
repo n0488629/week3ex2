49af19d34357335a3f4928e337d16095bf3e0e33
Nick <Nick@DESKTOP-82IOVQF>
2025-01-30 17:32:27 -0600

    Add project files.

---
+++ week3ex2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3ex2
+{
+    internal class Program
+    {
+        // One single random generator to be called twice, I had 2 different ones in each Rolldice method but that did not work because they seed at the same time
+        public static Random rnd = new Random(); // create a random seed method 
+        public static int RollDice1() // first roll dice method
+        {      
+            
+            return (rnd.Next(1, 7)); // return random value
+        }
+        public static int RollDice2() // second roll dice method
+        {
+           
+            return (rnd.Next(1, 7)); // return a second random value
+        }
+        static void Main(string[] args)
+        {
+            bool again = true; // bool for while loop to keep program running
+            Random rnd = new Random();
+            // Program title
+            Console.WriteLine("Roll the Dice program!");
+            
+            while (again == true) // while loop to keep program running
+            {
+                // display first dice roll
+                
+                    Console.WriteLine("First dice rolled: " + RollDice1());
+                
+                // display second dice roll
+                Console.WriteLine("Second dice rolled: " + RollDice2());
+                Console.WriteLine("Would you like to roll the dice again? Y or N?");
+                string input = Console.ReadLine();
+                if (input.ToLower().StartsWith("n")) // if user inputs "n", assign false to again variable to end loop
+                {
+                    again = false;
+                }
+            } 
+            
+        }
+    }
+}
---
+++ week3ex2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("week3ex2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("week3ex2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ae886003-acb2-4233-b991-8d5ba861555c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
